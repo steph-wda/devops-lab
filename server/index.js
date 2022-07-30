5f52a1812bc78c18df996b92839d264d53fd9518
Stephanie Wilks <wilksstephanie.career@outlook.com>
2022-07-30 16:25:47 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require("cors")
+
+const{SERVER_PORT}=process.env
+
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
+
