0177c38ba41e096f4dc2e78c13cd846956558201
Stephanie Wilks <wilksstephanie.career@outlook.com>
2022-07-30 16:36:18 -0400

    deploying app

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
+const path = require('path')
 require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require("cors")
 
-const{SERVER_PORT}=process.env
 
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, "/../public")));
 
+app.get("/", (req, res) => {
+    rollbar.info(`user accessed the main page`)
+    res.sendFile(path.resolve("public/landing.html"));
+  });
+
+const SERVER_PORT = process.env.PORT || 4400
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
 
